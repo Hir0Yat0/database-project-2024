a52cba19258c9060e7b3b265bd315bdddad7daa1
Hir0Yat0 <xToppyThailandx@gmail.com>
2024-03-29 23:37:21 +0700

    fix: solved(?) merge conflict

--- src/sql/init-tables.sql
+++ src/sql/init-tables.sql
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-=======
+
 -- Drop existing tables if they exist
 drop table if exists "Role" cascade;
 
@@ -24,7 +23,6 @@ drop table if exists "Equipment Company" cascade;
 
 -- Create Billing table
 CREATE TABLE "Billing" (
->>>>>>> 1401ac7231f4dbdb64331a9249005059b05b6ecd
   "Billing ID" SERIAL PRIMARY KEY,
   "Card Number" VARCHAR(16),
   "Expiry" TIMESTAMP,
@@ -48,42 +46,30 @@ CREATE TABLE "User" (
   FOREIGN KEY ("Billing ID") REFERENCES "Billing" ("Billing ID")
 );
 
-<<<<<<< HEAD
-=======
--- Create Role table
 
+-- Create Role table
 CREATE TABLE "Role" (
->>>>>>> 1401ac7231f4dbdb64331a9249005059b05b6ecd
   "Role ID" SERIAL PRIMARY KEY,
   "Role Name" VARCHAR(255)
 );
 
-<<<<<<< HEAD
-=======
 -- Create Package table
 CREATE TABLE "Package" (
->>>>>>> 1401ac7231f4dbdb64331a9249005059b05b6ecd
   "Package ID" SERIAL PRIMARY KEY,
   "Price" DECIMAL(10,2),
   "Package Name" VARCHAR(255)
 );
 
-<<<<<<< HEAD
-=======
 -- Create Customer table
 CREATE TABLE "Customer" (
->>>>>>> 1401ac7231f4dbdb64331a9249005059b05b6ecd
   "Customer ID" SERIAL PRIMARY KEY,
   "User ID" INT,
   "Membership ID" INT DEFAULT NULL,
   FOREIGN KEY ("User ID") REFERENCES "User" ("User ID")
 );
 
-<<<<<<< HEAD
-=======
 -- Create Membership table
 CREATE TABLE "Membership" (
->>>>>>> 1401ac7231f4dbdb64331a9249005059b05b6ecd
   "Membership ID" SERIAL PRIMARY KEY,
   "Customer ID" INT,
   "Package ID" INT,
@@ -92,7 +78,6 @@ CREATE TABLE "Membership" (
   FOREIGN KEY ("Package ID") REFERENCES "Package" ("Package ID")
 );
 
-<<<<<<< HEAD
 CREATE TABLE "Staff" (
   "Staff ID" SERIAL PRIMARY KEY,
   "User ID" INT,
@@ -101,7 +86,6 @@ CREATE TABLE "Staff" (
   FOREIGN KEY ("User ID") REFERENCES "User" ("User ID"),
   FOREIGN KEY ("Role ID") REFERENCES Role ("Role ID"),
   FOREIGN KEY ("Branch ID") REFERENCES "Branch" ("Branch ID")
-=======
 
 -- Create Equipment Type table
 CREATE TABLE "Equipment Type" (
@@ -109,19 +93,14 @@ CREATE TABLE "Equipment Type" (
   "Name" VARCHAR(255) UNIQUE
 );
 
-<<<<<<< HEAD
-=======
 -- Create Branch table
 CREATE TABLE "Branch" (
->>>>>>> 1401ac7231f4dbdb64331a9249005059b05b6ecd
   "Branch ID" SERIAL PRIMARY KEY,
   "Address" VARCHAR(255),
   "Manager ID" INT,
   FOREIGN KEY ("Manager ID") REFERENCES "Staff" ("Staff ID")
 );
 
-<<<<<<< HEAD
-=======
 -- Create Equipment Company table
 CREATE TABLE "Equipment Company" (
   "Company ID" SERIAL PRIMARY KEY,
@@ -131,7 +110,6 @@ CREATE TABLE "Equipment Company" (
 
 -- Create Equipment table
 CREATE TABLE "Equipment" (
->>>>>>> 1401ac7231f4dbdb64331a9249005059b05b6ecd
   "Equipment ID" SERIAL PRIMARY KEY,
   "Name" VARCHAR(255),
   "Purchase Date" TIMESTAMP,
@@ -140,9 +118,6 @@ CREATE TABLE "Equipment" (
   "Equipment Type ID" VARCHAR(255),
   "Branch ID" INT,
   FOREIGN KEY ("Company ID") REFERENCES "Equipment Company" ("Company ID"),
-<<<<<<< HEAD
   FOREIGN KEY ("Equipment Type ID") REFERENCES "Equipment Type" ("ID"),
   FOREIGN KEY ("Branch ID") REFERENCES "Branch" ("Branch ID")
-=======
->>>>>>> 1401ac7231f4dbdb64331a9249005059b05b6ecd
 );
