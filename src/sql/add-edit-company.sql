842cecc3ddeb76fb3616ff7ecedc7559dbf0dc89
Anish Khanijaur <anishkhanijaur089@gmail.com>
2024-03-30 12:07:56 +0700

    feat: added add-edit-company.sql

---
+++ src/sql/add-edit-company.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE PROCEDURE add_company(IN name VARCHAR(255), IN phone_number VARCHAR(15), INOUT status_code INT) AS
+$$
+BEGIN
+    INSERT INTO public."Equipment Company"("Name", "Phone Number") VALUES (name, phone_number);
+    COMMIT;
+
+    status_code := 0; -- Assigning 0 to status_code
+END;
+$$ LANGUAGE plpgsql;
+
+CALL add_company('Cool Company', '555-555-6666', 0);
+
+CREATE OR REPLACE PROCEDURE edit_company(IN company_id INT, IN name VARCHAR(255), IN phone_number VARCHAR(15), INOUT status_code INT) AS
+$$
+BEGIN
+    UPDATE public."Equipment Company"
+        SET "Name" = name,
+            "Phone Number" = phone_number
+        WHERE "Company ID" = company_id;
+    COMMIT;
+    status_code := 0; -- Assigning 0 to status_code
+END;
+$$ LANGUAGE plpgsql;
+
+CALL edit_company(3, 'Cooler Company', '555-555-6666', 0);
